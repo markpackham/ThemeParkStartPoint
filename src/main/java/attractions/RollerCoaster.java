ff1e2f42dc5dd510edeb0642c1b95f5ee8a8dc46
mark <markpackham1@gmail.com>
2020-06-23 16:45:43 +0100

    Initial commit

---
+++ src/main/java/attractions/RollerCoaster.java
@@ -0,0 +1,16 @@
+package attractions;
+
+import behaviours.ISecurity;
+import people.Visitor;
+
+public class RollerCoaster  extends Attraction implements ISecurity {
+
+    public RollerCoaster(String name, int rating) {
+        super(name, rating);
+    }
+
+    @Override
+    public boolean isAllowedTo(Visitor visitor) {
+        return false;
+    }
+}
---
+++ src/main/java/stalls/TobaccoStall.java
@@ -0,0 +1,21 @@
+package stalls;
+
+import behaviours.ISecurity;
+import people.Visitor;
+
+public class TobaccoStall extends Stall implements ISecurity {
+
+    public TobaccoStall(String name, String ownerName, ParkingSpot parkingSpot) {
+        super(name, ownerName, parkingSpot);
+    }
+
+    @Override
+    public int getRating() {
+        return 0;
+    }
+
+    @Override
+    public boolean isAllowedTo(Visitor visitor) {
+        return false;
+    }
+}
