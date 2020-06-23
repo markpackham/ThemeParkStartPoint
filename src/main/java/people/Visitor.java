c91b6c014fdf65aff06f4375809a53a7a229bf99
mark <markpackham1@gmail.com>
2020-06-23 17:47:47 +0100

    Test can add visit to vistors

--- src/main/java/people/Visitor.java
+++ src/main/java/people/Visitor.java
@@ -1,15 +1,22 @@
 package people;
 
+import attractions.Attraction;
+import behaviours.IReviewed;
+
+import java.util.ArrayList;
+
 public class Visitor {
 
     private int age;
     private double height;
     private double money;
+    private ArrayList<Attraction> visitedAttractions;
 
     public Visitor(int age, double height, double money) {
         this.age = age;
         this.height = height;
         this.money = money;
+        this.visitedAttractions = new ArrayList<Attraction>();
     }
 
     public int getAge() {
@@ -22,5 +29,13 @@ public class Visitor {
 
     public double getMoney() {
         return money;
+    }
+
+    public void addVisit(Attraction visit){
+        this.visitedAttractions.add(visit);
+    }
+
+    public ArrayList<Attraction> getVisitedAttractions(){
+        return visitedAttractions;
     }
 }
--- src/test/java/people/VisitorTest.java
+++ src/test/java/people/VisitorTest.java
@@ -1,5 +1,8 @@
 package people;
 
+import attractions.Dodgems;
+import attractions.Park;
+import attractions.Playground;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,10 +11,16 @@ import static org.junit.Assert.assertEquals;
 public class VisitorTest {
 
     Visitor visitor;
+    Dodgems dodgems;
+    Park park;
+    Playground playground;
 
     @Before
     public void before(){
         visitor = new Visitor(14, 1.2, 40.0);
+        dodgems = new Dodgems("Bumper Cars", 5);
+        park = new Park("Leafy Meadows", 9);
+        playground = new Playground("Fun Zone", 7);
     }
 
     @Test
@@ -27,5 +36,13 @@ public class VisitorTest {
     @Test
     public void hasMoney() {
         assertEquals(40.0, visitor.getMoney(), 0.1);
+    }
+
+    @Test
+    public void getVisitedAttractions() {
+        visitor.addVisit(dodgems);
+        visitor.addVisit(park);
+        visitor.addVisit(playground);
+        assertEquals(3, visitor.getVisitedAttractions().size());
     }
 }
